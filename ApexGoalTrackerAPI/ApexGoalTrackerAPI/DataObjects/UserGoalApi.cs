f3cd9511662350e86dfa55fd9dc6d3ca60864a15
EMatsonE <90651995+EMatsonE@users.noreply.github.com>
2022-04-04 20:26:16 -0500

    test

---
+++ ApexGoalTrackerAPI/ApexGoalTrackerAPI/DataObjects/UserGoalApi.cs
@@ -0,0 +1,14 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ApexGoalTrackerAPI.DataObjects
+{
+    public class UserGoalApi
+    {
+        public string ApexID { get; set; }
+        public int RankScore { get; set; }
+        public string RankName { get; set; }
+    }
+}
