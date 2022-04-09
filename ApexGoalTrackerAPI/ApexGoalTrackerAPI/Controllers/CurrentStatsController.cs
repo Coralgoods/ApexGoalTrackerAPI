08e39ca224454936461590bd00852881b38ad9c8
Coralgoods <91501799+Coralgoods@users.noreply.github.com>
2022-04-09 17:56:36 -0400

    4_9V2

--- ApexGoalTrackerAPI/ApexGoalTrackerAPI/Controllers/CurrentStatsController.cs
+++ ApexGoalTrackerAPI/ApexGoalTrackerAPI/Controllers/CurrentStatsController.cs
@@ -31,8 +31,6 @@ namespace ApexGoalTrackerAPI.Controllers
             {               
                 //currentStats.allCurrentStats = context.currentStats.ToList(); //Works for all records
                 currentStats.allCurrentStats = context.currentStats.Where(u => u.ApexID == userApi.ApexID).ToList();
-
-
             }
             return currentStats; 
         }
